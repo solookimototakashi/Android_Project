695bfa94f4a556293afbae82df19f296c4ba0f81
solookimototakashi <solookimototakashi@gmail.com>
2022-11-24 09:52:14 +0900

    Home

--- TEST/main.py
+++ TEST/main.py
@@ -1,4 +1,7 @@
 # --- Kivy 関連 --- #
+#1 各種インポート
+import os
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
 from kivy.app import App
 from kivy.config import Config
 
@@ -19,6 +22,8 @@ import learning
 import cv2
 import os
 import matplotlib.pyplot as plt
+from kivy.graphics import (Canvas, Translate, Fbo, ClearColor,
+                           ClearBuffers, Scale)
 
 import Read_Func
 
@@ -55,6 +60,8 @@ class MyPaintApp(App):
         # 学習を行う
         # self.model = learning.learn_MNIST()
         self.model = learning.load_MNIST()
+        self.model.summary()
+
 
     def build(self):
         self.painter = MyCanvasWidget()
@@ -67,29 +74,10 @@ class MyPaintApp(App):
         self.painter.ids['paint_area'].set_color()  # クリアした後に色を再びセット
 
     def predict(self):
-        self.painter.export_to_png('canvas.png')  # 画像を一旦保存する
-
-        # Pillowで読み込み、余計な部分を切り取る。またグレースケールにも変換
-        # image = Image.open('canvas.png').crop((0, 0, 600, 600)).convert('L')
-        # リサイズ
-        # image = image.resize((14, 14))
-        # image = np.array(image)
-        # image = image.reshape(1, 196)
-        # ans = self.model.predict(image)
-        # print('This Digit is ... ', np.argmax(ans))
-
-        # Xt = []
-        # img = cv2.imread("canvas.png", 0)
-        # img = cv2.resize(img,(28, 28), cv2.INTER_CUBIC)
-
-        # Xt.append(img)
-        # Xt = np.array(Xt)/255
-
-        # result = self.model.predict(Xt)
-        # print(result[0])        
+        self.painter.export_to_png('canvas.png',float=0.1)  # 画像を一旦保存する       
 
         #2 各種設定  
-        train_data_path = os.path.join(os.path.dirname(__file__),'Japanese_text_dataset/') # ここを変更。
+        train_data_path = os.path.join(os.path.dirname(__file__),'Japanese_text_dataset') # ここを変更。
         for current_dir, sub_dirs, files_list in os.walk(train_data_path):
             if current_dir == train_data_path:
                 fol_list = [os.path.join(current_dir,sub_dirs_item) for sub_dirs_item in sub_dirs]
@@ -98,9 +86,16 @@ class MyPaintApp(App):
         image_height = 14  # ここを変更
         color_setting = 3  # ここを変更。利用する学習済みモデルのカラー形式と同じにする
         if color_setting == 1:
-            img = cv2.imread("canvas.png", 0)   
+            img = cv2.imread("canvas.png", 0)
+            h, w, c = img.shape
+            img2 = img[0 : int(h*0.85), 0 : w]
+            cv2.imwrite("canvas.png", img2)
+            img = cv2.imread("canvas.png", 0)                  
         elif color_setting == 3:
             img = cv2.imread("canvas.png", 1)
+            h, w, c = img.shape
+            img2 = img[0 : int(h*0.85), 0 : w]
+            cv2.imwrite("canvas.png", img2)               
             img = cv2.resize(img, (image_width, image_height))
             plt.imshow(img)
         if color_setting == 1:
@@ -110,20 +105,21 @@ class MyPaintApp(App):
             plt.show()
 
         img = img.reshape(image_width, image_height, color_setting).astype('float32')/255 
+        re_img = np.array([img])
 
         #5 予測と結果の表示等
 
-        prediction = self.model.predict(np.array([img]))
+        prediction = self.model.predict(re_img)
         result = prediction[0]
 
         for i, accuracy in enumerate(result):
-            print('画像認識AIは「', os.path.basename(folder[i]), '」の確率を', int(accuracy * 100), '% と予測しました。')
+            print('画像認識AIは「', os.path.basename(fol_list[i]), '」の確率を', int(accuracy * 100), '% と予測しました。')
 
             print('-------------------------------------------------------')
-            print('予測結果は、「', os.path.basename(folder[result.argmax()]),'」です。')
-            print(' \n\n　＊　「確率精度が低い画像」や、「間違えた画像」を再学習させて、オリジナルのモデルを作成してみてください。')
-            print(' \n　＊　「間違えた画像」を数枚データセットに入れるだけで正解できる可能性が向上するようでした。')
-            print(' \n　＊　何度も実行すると「WARNING:tensorflow」が表示されますが、結果は正常に出力できるようでした。')
+            print('予測結果は、「', os.path.basename(fol_list[result.argmax()]),'」です。')
+            # print(' \n\n　＊　「確率精度が低い画像」や、「間違えた画像」を再学習させて、オリジナルのモデルを作成してみてください。')
+            # print(' \n　＊　「間違えた画像」を数枚データセットに入れるだけで正解できる可能性が向上するようでした。')
+            # print(' \n　＊　何度も実行すると「WARNING:tensorflow」が表示されますが、結果は正常に出力できるようでした。')
 
 if __name__ == '__main__':
     Window.clearcolor = get_color_from_hex('#000000')   # ウィンドウの色を黒色に変更する
